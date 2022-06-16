3d58b93f8152473eefe8571b87fa083d7599b489
bob <99412612+13lambertj@users.noreply.github.com>
2022-06-16 11:08:52 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello i AM JARVIS")
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Ghost)
+})
